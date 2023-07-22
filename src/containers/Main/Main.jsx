0548fe4587c49abe9244396c2254789fb9438659
Claire Froelich <claire.froelich@gmail.com>
2023-07-22 16:01:36 -0400

    add main popup

---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import icon from '../../assets/img/icon-128.png';
+
+const Main = () => {
+  return (
+    <div>
+      <p>Hello, philosopher!</p>
+      <img src={icon} alt="extension icon" />
+    </div >
+  )
+}
+
+export default Main;
